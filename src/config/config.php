4b3390243d9ba001ed5deec9125fba8c48c860ca
Luana Carvalho <60821298+luanafluffy@users.noreply.github.com>
2020-06-08 12:51:23 -0300

    Manipulação de classes e seus atributos
    Aula 277 finalizada - Método para Atualizar (Update)

--- src/config/config.php
+++ src/config/config.php
@@ -2,6 +2,9 @@
 date_default_timezone_set('America/Recife');
 setlocale(LC_TIME, 'pt_BR', 'pt_BR.utf-8', 'portuguese');
 
+//Constantes Gerais
+define('DAILY_TIME', 60 * 60 *8);
+
 //Pastas
 define('MODEL_PATH', realpath(dirname(__FILE__) . '/../models'));
 define('VIEW_PATH', realpath(dirname(__FILE__) . '/../views'));
@@ -13,6 +16,8 @@ define('EXCEPTION_PATH', realpath(dirname(__FILE__) . '/../exceptions'));
 require_once(realpath(dirname(__FILE__) . '/database.php'));
 require_once(realpath(dirname(__FILE__) . '/loader.php'));
 require_once(realpath(dirname(__FILE__) . '/session.php'));
+require_once(realpath(dirname(__FILE__) . '/date_utils.php'));
+require_once(realpath(MODEL_PATH . '/Model.php'));
 require_once(realpath(MODEL_PATH . '/Model.php'));
 require_once(realpath(MODEL_PATH . '/User.php'));
 require_once(realpath(EXCEPTION_PATH . '/AppException.php'));
--- src/controllers/day_records.php
+++ src/controllers/day_records.php
@@ -2,6 +2,15 @@
 session_start();
 requireValidSession();
 
+loadModel('WorkingHours');
+
 $date = (new Datetime())->getTimestamp();
 $today = strftime('%d de %B de %Y', $date);
-loadTemplateView('day_records', ['today' =>$today]);
+
+$user = $_SESSION['user'];
+$records = WorkingHours::loadFromUserAndDate($user->id, date('Y-m-d'));
+
+loadTemplateView('day_records', [
+    'today' =>$today,
+    'records' => $records
+]);
